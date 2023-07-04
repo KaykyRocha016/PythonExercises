79b2c618d0851c9b78c6a998e1d58a367c6e6201
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 15:35:11 -0300

    Update A13.py

--- A/A13.py
+++ A/A13.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia um número inteiro S que representa uma quantidade de segundos. Seu programma deve imprimir quatro valores inteiros, que representem a quantidade de segundos S em dias, horas, minutos e segundos. Por exemplo, 188365 segundos representam 2 dias, 4 horas, 19 minutos e 25 segundos. Dica: lembre-se dos operadores resto e divisão inteira.
 a = int(input())
 segundosPdia = 86400
 segundosPhora = 3600
