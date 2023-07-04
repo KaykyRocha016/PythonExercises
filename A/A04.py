b74efbd67a0af851c793e9b74f193faea1ab4a5f
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:19:09 -0300

    Update A04.py

--- A/A04.py
+++ A/A04.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia o nome de um(a) aluno(a). Após isso, o programa deve ler duas notas de provas. O programa deve calcular da média simples delas e escrever a saída conforme modelo abaixo. A média deve ser escrita com duas casas decimais.
 nome = input()
 a = float(input())
 b = float(input())
