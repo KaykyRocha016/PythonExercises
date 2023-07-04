c3cae14fb35105305fdf9f28d2e61ee4192b9b33
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:17:36 -0300

    A02.py

--- A/A02.py
+++ A/A02.py
@@ -1,4 +1,4 @@
-Faça um programa que leia dois números inteiros e imprima o valor da soma deles, a diferença do primeiro pro segundo e a multiplicação entre eles.
+#Faça um programa que leia dois números inteiros e imprima o valor da soma deles, a diferença do primeiro pro segundo e a multiplicação entre eles.
 a = int(input())
 b = int(input())
 print(a + b)
