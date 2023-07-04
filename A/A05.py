d2ae7347fd3d278fc6b4dd1baa8471a983fcbdd4
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:19:41 -0300

    Update A05.py

--- A/A05.py
+++ A/A05.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia dois números inteiros. Após isso, seu programa deve imprimir o resultado das seguintes operações: 1) o valor da divisão real do primeiro número lido pelo segundo (imprimir com duas casas decimais); 2) o valor da divisão inteira do primeiro pelo segundo (imprimir como número inteiro); 3) o valor do resto da divisão do primeiro pelo segundo (imprimir como número inteiro).
 a = int(input())
 b = int(input())
 divisão1 = float(a/b)
