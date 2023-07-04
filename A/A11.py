76b38d2e8c24e2c004f7ddabfe5469170b728d98
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 15:00:57 -0300

    Update A11.py

--- A/A11.py
+++ A/A11.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia o salário fixo de um vendedor e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 18% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês.
 salário = float(input())
 vendas = float(input())
 comissão = vendas * 0.18
