d7c34764544b5f3b50836b509e8f4c2ce127c5d2
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:22:22 -0300

    Update A08.py

--- A/A08.py
+++ A/A08.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia o valor dos catetos de um triângulo retângulo e calcule a hipotenusa, de acordo com o Teorema de Pitágoras. Pesquise como extrar a raiz quadrada de um número.
 cateto1 = float(input())
 cateto2 = float(input())
 hipotenusa =(cateto1**2 + cateto2**2)**(1/2)
