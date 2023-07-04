87f6fbdcc09a406d4754c0d20a1bacae397a93b2
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:23:18 -0300

    Update A09.py

--- A/A09.py
+++ A/A09.py
@@ -1,8 +1,9 @@
-A = float(input())
-B = float(input())
-C = float(input())
-delta = B**2 - 4 *A* C
-x1 = (-1*B + delta**(1/2))/(2*A)
-x2 = (-1*B - delta**(1/2))/(2*A)
+#Faça um programa que leia três números reais A, B e C de uma equação do segundo grau, considerando: Ax^2 + Bx + C. Seu programa deve calcular e imprimir as as raízes da equação. Assuma que delta sempre será positivo.
+a = float(input())
+b = float(input())
+c = float(input())
+delta = b**2 - 4 *a* c
+x1 = (-1*b + delta**(1/2))/(2*a)
+x2 = (-1*b - delta**(1/2))/(2*a)
 print(f"{x1:.2f}")
 print(f"{x2:.2f}")
