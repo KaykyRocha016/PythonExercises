24297c514e9817cde69b9f240d2803a36ace4419
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 15:37:22 -0300

    Update A15.py

--- A/A15.py
+++ A/A15.py
@@ -1,12 +1,13 @@
-E = int(input())
-B = int(input())
-N = int(input())
-V=int(input())
-brancos =(B/E)*(100)
-Nulos = (N/E)*100
-validos = (V/E)*100
-ausentes= 100 - (brancos + Nulos + validos)
-print("Nulos:",f"{Nulos:.2f}%")
+#Faça um programa que leia um número inteiro E de eleitores de um município, um número inteiro B que representa o número de votos brancos, um número N de votos nulos e um número V de votos válidos. O programa deve calcular e escrever o percentual que cada um representa em relação ao total de eleitores, além da porcentagem de ausentes. 
+e = int(input())
+b = int(input())
+n = int(input())
+v=int(input())
+brancos =(b/e)*(100)
+nulos = (n/e)*100
+validos = (v/e)*100
+ausentes= 100 - (brancos + nulos + validos)
+print("Nulos:",f"{nulos:.2f}%")
 print("Brancos:",f"{brancos:.2f}%")
 print("Validos:",f"{validos:.2f}%")
 print("Ausentes:",f"{ausentes:.2f}%")
