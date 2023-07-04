6f745a93d0c45bac0b91599df061006b60f36b61
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:20:08 -0300

    Update A06.py

--- A/A06.py
+++ A/A06.py
@@ -1,3 +1,4 @@
+#Em épocas de crise, comerciantes estão oferecendo descontos para aumentarem suas vendas. Faça um programa que leia o valor total da compra e um valor de desconto (de 0 a 100, representando a porcentagem de desconto). O programa de escrever o valor da compra com o desconto aplicado. Escreva a saída com duas casas decimais.
 a = float(input())
 b = float(input())
 preço = a - a * b/100
