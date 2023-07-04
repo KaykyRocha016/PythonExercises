a10c82fe75e370516e7b4a3693ab412568f2ca2f
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 15:00:28 -0300

    Update A10.py

--- A/A10.py
+++ A/A10.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia uma temperatura em graus Celsius e converta e escreva o correspondente em graus Fahrenheit (pesquise como essa conversão é feita)
 celsius = float(input())
 fahrenheit= celsius * (9/5) + 32
 print(f"{fahrenheit:.2f}")
