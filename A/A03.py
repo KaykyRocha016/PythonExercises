d831a1d19109968d2d2da37733608958db139775
KaykyRocha016 <133814931+KaykyRocha016@users.noreply.github.com>
2023-07-04 14:18:28 -0300

    A03.py

--- A/A03.py
+++ A/A03.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia duas notas de provas e calcule e escreva a média simples delas. Escreva a saída com duas casas decimais.
 a = float(input())
 b = float(input())
 resultado = (a + b)/2
